af608a1a2137d3f6a8464d6e821cccc3bc524f4f
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-10-18 20:36:46 -0500

    Remove netns dependency

--- src/srl-vxlan-oam/ecmp-traceroute.py
+++ src/srl-vxlan-oam/ecmp-traceroute.py
@@ -13,7 +13,7 @@
 # See also: https://0xbharath.github.io/art-of-packet-crafting-with-scapy/network_recon/traceroute/index.html
 #
 
-import sys, os, netns, logging, ipaddress, json
+import sys, os, logging, ipaddress, json
 from scapy.layers.inet import IP, UDP, ICMP, traceroute
 from scapy.arch import get_if_addr, get_if_hwaddr
 from scapy.sendrecv import srp # send/recv at Layer2
@@ -47,10 +47,9 @@ if DEBUG:
 
 # Use scapy
 
-# Needs to run in srbase-default netns
+# Needs to run in srbase-default netns, assume caller takes care of that
 uplink_addr = {}
-with netns.NetNS(nsname="srbase-default"):
- for uplink in UPLINKS:
+for uplink in UPLINKS:
     local_ip = get_if_addr(uplink)
     local_mac = get_if_hwaddr(uplink)
     d = int(local_ip[-1])
@@ -68,20 +67,17 @@ IANA_TRACERT_PORT = 33434
 # ans.summary( lambda s,r : r.sprintf("%IP.src%\t{UDP:%UDP.sport%}") )
 results = {} # Indexed by VTEP
 done = {}
-with netns.NetNS(nsname="srbase"):
 
- # Need to listen for ICMP replies on every uplink
- uplink_socks = {}
- filter = "(icmp and (icmp[0]=3 or icmp[0]=4 or icmp[0]=5 or icmp[0]=11 or icmp[0]=12))"
- for uplink in UPLINKS:
-   base_if = uplink.split('.')[0]
-   iface = resolve_iface( base_if )
+# Need to listen for ICMP replies on every uplink
+uplink_socks = {}
+filter = "(icmp and (icmp[0]=3 or icmp[0]=4 or icmp[0]=5 or icmp[0]=11 or icmp[0]=12))"
+for uplink in UPLINKS:
+   iface = resolve_iface( uplink )
    s = iface.l2socket()(iface=iface,filter=filter,type=ETH_P_ALL)
    uplink_socks[ s ] = uplink
 
- for ttl in range(1,5):
+for ttl in range(1,5):
   for u,uplink in enumerate(UPLINKS):
-   base_if = uplink.split('.')[0]
    macs = uplink_addr[ uplink ]
    l2 = Ether(src=macs['src_mac'],dst=macs['dst_mac'])
    for vtep in VTEP_IPs:
@@ -111,8 +107,8 @@ with netns.NetNS(nsname="srbase"):
      # Add 500ms interval to avoid rate limiting on unlicensed SRL? Slows down everything
      # ans = srp1()
      logging.info( f"Sending {udp_src_hi-udp_src_lo+1} packets to {vtep} TTL={ttl}" )
-     ans, unans = srp(trace_pkts, iface=base_if, verbose=DEBUG, inter=TIMEOUT,
-                rcv_pks=uplink_socks,timeout=3*TIMEOUT,retry=1)
+     ans, unans = srp(trace_pkts, iface=uplink, verbose=DEBUG, inter=TIMEOUT,
+                rcv_pks=uplink_socks,timeout=3*TIMEOUT,retry=0)
      logging.info( f"Got responses: {ans} no answer={unans}" )
      if DEBUG:
          print( f"TTL={ttl} {uplink} VTEP={vtep}: Answers: {ans} missing: {unans}" )
@@ -147,8 +143,8 @@ with netns.NetNS(nsname="srbase"):
         results[vtep] = { ttl: next_hops, 'unanswered' : 0 }
      results[vtep]['unanswered'] += len(unans)
 
- # Done
- for s in uplink_socks:
+# Done
+for s in uplink_socks:
     s.close()
 
 print( json.dumps(results) )
